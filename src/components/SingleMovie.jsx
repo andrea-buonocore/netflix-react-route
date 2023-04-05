f1b52c3807651bb2c3d7a72528422f766397c005
andrea-buonocore <123959335+andrea-buonocore@users.noreply.github.com>
2023-04-05 17:30:17 +0200

    added detail button

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -2,71 +2,70 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { Component } from "react";
 import { Link } from "react-router-dom";
-
+import { Button } from "react-bootstrap";
 class SingleMovie extends Component {
 
     state = {
         isShown: false,
     }
 
-    
+
     render() {
-    
+
         return (
             <Col className="my-2">
-                <Link to={`/movies/${this.props.movie.imdbID}`}>
-                    <Card className="border border-1 border-dark movieCard">
-                        <Card.Img
-                            variant="top"
-                            className="cardImg"
-                            src={this.props.movie.Poster}
-                            onMouseEnter={
+
+                <Card className="border border-1 border-dark movieCard">
+                    <Card.Img
+                        variant="top"
+                        className="cardImg"
+                        src={this.props.movie.Poster}
+                        onMouseEnter={
+                            () => {
+
+                                this.setState({
+                                    isShown: true
+                                })
+                            }
+                        }
+                        onMouseOut={
+                            () => {
+                                this.setState({
+                                    isShown: false
+                                })
+                            }
+                        }
+                    />
+
+                    {this.state.isShown && (
+                        <div>
+                            <Card.Body className="py-0 px-2" onMouseEnter={
                                 () => {
 
                                     this.setState({
                                         isShown: true
                                     })
                                 }
-                            }
-                            onMouseOut={
-                                () => {
-                                    this.setState({
-                                        isShown: false
-                                    })
-                                }
-                            }
-                        />
+                            }>
+                                <Card.Text className="text-dark fw-bold text-truncate">
+                                    <small>{this.props.movie.Title}</small>
+                                    <br />
+                                    <small className="text-muted">{this.props.movie.Year}</small>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="d-flex align-items-center justify-content-between m-0">
+                                <i className="bi bi-play-circle-fill fs-2"></i>
+                                <Link to={`/movies/${this.props.movie.imdbID}`}>
+                                    <Button variant="primary">Details</Button>
+                                </Link>
+                            </Card.Footer>
 
-                        {this.state.isShown && (
-                            <div>
-                                <Card.Body className="py-0 px-2" onMouseEnter={
-                                    () => {
-
-                                        this.setState({
-                                            isShown: true
-                                        })
-                                    }
-                                }>
-                                    <Card.Text className="text-dark fw-bold text-truncate">
-                                        <small>{this.props.movie.Title}</small>
-                                        <br />
-                                        <small className="text-muted">{this.props.movie.Year}</small>
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer
-                                    className="d-flex align-items-center m-0"
-
-                                >
-                                    <i className="bi bi-play-circle-fill"></i>
-                                    <small className="text-dark mx-2">Watch Now</small>
-                                </Card.Footer>
-
-                            </div>
-                        )}
+                        </div>
+                    )}
 
 
-                    </Card>
-                </Link>
+                </Card>
+
             </Col>
         )
     }
